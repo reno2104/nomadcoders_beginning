043d76dbe7e17c53bc046382249e163ae78df956
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-01-22 02:21:01 +0900

    if절 추가

---
+++ Python/if절.py
@@ -0,0 +1,17 @@
+## if, else, elif
+
+password_correct = False
+
+if password_correct:
+  print("Here is your money")
+else:
+  print("Wrong password")
+
+
+winner = 20
+if winner > 10:
+  print("winner is greater than 10")
+elif winner < 10:
+  print("winner is less than 10")
+else:
+  print("winner is 10")
