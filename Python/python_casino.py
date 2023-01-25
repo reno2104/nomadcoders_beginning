064664d83ef68b9faae37e72c72280c711163e19
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-01-25 12:47:09 +0900

    while문을 활용해 파이썬 카지노 만들기

--- Python/python_casino.py
+++ Python/python_casino.py
@@ -1,5 +1,5 @@
+***
 from random import randint
-#random.randint(a,b) : 첫번째 파라미터 a는 N보다 작거나 같고, N은 두번째 파라미터 b보다 작거나 같다.
 
 print("Welcome to Python Casino!")
 pc_choice = randint(1, 50)
@@ -16,4 +16,31 @@ while playing:
           elif user_choice < pc_choice:
             print("Higher! ")
 
-# import없이 모듈종류 알아보는 방법  https://docs.python.org/3/library/functions.html
+***
+
+
+from random import randint
+print("Welcome to Python Casino")
+print("You have 10 chances")
+
+pc_choice = randint(1, 50)
+
+playing = True
+chance = 10
+
+while playing:
+  chance = chance - 1
+if chance >= 0:
+  user_choice = int(input("Choose number."))
+if user_choice == pc_choice:
+  print("You won!")
+  playing = False
+elif user_choice > pc_choice:
+  print("Lower!")
+  print("You have", chance, "chances")
+elif user_choice < pc_choice:
+  print("Higher!")
+  print("You have", chance, "chances")
+elif chance < 0:
+  print("Game over")
+  playing = False
