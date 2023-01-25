aae92af52e56c2b5a25d20ff5bc7ccbf9617d422
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-01-25 14:50:06 +0900

    DATA STRUCTURES

---
+++ Python/method.py
@@ -0,0 +1,30 @@
+"""
+data structure는 data를 구조화 하고 싶을 때 사용하는 것이다.
+
+python에서 배워야할 data structure는 3가지가 있다.
+
+1. list
+2. tuple
+3. dictionary
+
+list를 사용하는 방법은 variable를 만들고 값에 [ ] 대활호를 입력하면된다. 대과호 안에는 data를 넣고 쉼표 , 를 사용해 data를 분리할 수 있다.
+
+근데 list를 print를 하면 list의 값이 그대로 나온다. 그 이유는 method를 사용하지 않았기 때문이다.
+
+print ( ) 소괄호 안에 string이나 숫자를 입력하고 점 . 을 쳐보면 많고 다양한 function이 보인다. 그 이유는 string이나 숫자 내부에는 많은 function와 결합이 되어 있기 때문이다.
+
+내부에 있는 function들은 function이라고 하지 않고 method이라고 한다. data뒤에 결합이나 연결되어있는 function은 method이라고 생각하면된다
+  
+"""
+
+#days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+name = "reno"
+# print(name.upper()) - 문자열 모두 대문자
+# print(name.capitalize()) - 첫글자만 대문자
+# print(name.startswith("n")) - 첫글자가 n이 맞으면 true, 틀리면 false
+# print(name.replace("o","😂")) - 해당글자를 ~로 대체하기.
+
+# 파이썬 표준 라이브러리 : https://docs.python.org/ko/3.8/tutorial/classes.html#method-objects
+# string : https://docs.python.org/ko/3.8/library/string.html
+# 읽어보면 좋은 class : https://docs.python.org/ko/3.8/tutorial/classes.html
