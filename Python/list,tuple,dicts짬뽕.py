1f7c53427f250912db439ee559656fecf0ea5788
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-01-27 12:47:11 +0900

    list, tuple, dicts 짬뽕

---
+++ Python/list,tuple,dicts짬뽕.py
@@ -0,0 +1,19 @@
+
+player = {
+  'name': 'reno', 
+  'age': 12, 
+  'alive': True,
+  'fav_food': ['🍕', '🍔']
+  'friend' : {
+  'name': 'bobo'
+  'fav_food': ['💋']
+  }
+}
+
+player['fav_food'] ='🥩'
+player.pop('alive')
+player['friend']
+['fav_food'].append('🍟')
+# friend 안의 fav_food에 접근
+
+print(player)
