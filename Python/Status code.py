a323954dfa9c2bd858e14652d56cf1793e97f2d3
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-01-31 02:06:53 +0900

    pypl로 받은 모듈로 웹사이트 구동여부 살펴보기

---
+++ Python/Status code.py
@@ -0,0 +1,28 @@
+from requests import get
+
+websites = (
+  "google.com", 
+  "naver.com",   
+  "https://nate.com", 
+  "daum.net",
+  "https://youtube.com"
+)
+
+results = {
+  
+}
+
+for website in websites:
+  if not website.startswith("https://"):
+
+    website = f"https://{website}"
+response = get(website)
+# print(response.status_code)
+#웹사이트가 성공적으로 응답하는지 알수있는 코드.
+if response.status_code == 200:
+  # print(f"{website} is ok")
+  results[website] = "Ok"
+else: 
+ # print(f"{website} is not ok")
+  results[website] = "Failed"
+print(results)
