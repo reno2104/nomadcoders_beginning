6329f4ec58252047a9f93c7d291e965783438f71
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-02-08 14:37:53 +0900

    weworkremotely_잡스크래퍼 수정

--- Python/weworkremotely_jobscrapper.py
+++ Python/weworkremotely_jobscrapper.py
@@ -13,37 +13,33 @@ search_term = "python"
 
 # 요청후 응답 받기
 response = get(f"{base_url}{search_term}")
-
 # 포트가 200이 아니라면,
 if response.status_code != 200:
   print("Can't request website")
 
 else:
+  # 결과에서 원하는 데이터 추출
+  results = []
   # html을 파싱하여 결과 받기
-  soup = BeautifulSoup(response.text, 'html.parser')
-
-# 결과에서 원하는 데이터 추출
-results = []
-
-jobs = soup.find_all('section', class_="jobs")
-for job_section in jobs:
-  posts = job_section.find_all('li')
-  posts.pop()  # 마지막 요소 제거, -1 기재해도 같은 결과
-for post in posts:
-  anchors = post.find_all('a')
-
-anchor = anchors[1]
-
-company, kind, region = anchor.find_all('span', class_=("company"))
-title = anchor.find('span', class_=("title"))
-
-data_result = {
-  'title': title.string,
-  'company': company.string,
-  'kind': kind.string,
-  'region': region.string
+  soup = BeautifulSoup(response.text, "html.parser")
+  jobs = soup.find_all('section', class_="jobs")
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    job_posts.pop()  # 마지막 요소 제거, -1 기재해도 같은 결과
+    for post in job_posts:
+     anchors = post.find_all('a')
+     anchor = anchors[1]
+     link = anchor['href']
+      
+     company, kind, region = anchor.find_all('span', class_=("company"))
+     title = anchor.find('span', class_=("title"))
+     data_result = {
+        'link' : f"https://weworkremotely.com{link}",
+        #'title': title.string,
+        'company': company.string,
+        'kind': kind.string,
+        'region': region.string
 }
-
 results.append(data_result)
 # print(company,kind,region,title)
 # link = anker['href'] # dicts 처럼 태그의 요소를 가져올 수 있다.
