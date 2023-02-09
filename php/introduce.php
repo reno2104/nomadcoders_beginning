d7756c090a2f43b002b8261efa5ae7629e10fe08
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-02-09 14:43:32 +0900

    php공부시작

---
+++ php/introduce.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$reno = "renovatio2104@naver.com";
+echo "my E-mail is $reno!";
+?>
+
+<?php
+$money = 2800000;
+$won = "won";
+echo "my pay is $money$won!";
+?>
+
+</body>
+</html>
